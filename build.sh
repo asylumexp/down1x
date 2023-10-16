72b43ea1a6a428e060d752a3fe32171628801b54
Sam Heinz <54530346+samh06@users.noreply.github.com>
2023-10-17 09:31:25 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ cat << EOF
                                ${GREEN}&&&&&&&&&&&%#&&%%%${NORMAL}
                         ${GREEN}&&&&&&&&&&&&&&&&&&%%#&&&%%%${NORMAL}
                 ${BLUE}&&&&&&&&${NORMAL}#############${GREEN}&&&%%&%%&&&&%%%${NORMAL}
-         ${BLUE}%%%%%%%%%%%%&&&${NORMAL}#  ${BLUE}down1x${NORMAL}  #${GREEN}%&&&&%%%%%%%%%%%%${NORMAL}
+         ${BLUE}%%%%%%%%%%%%&&&${NORMAL}#  ${BLUE}palen1x${NORMAL}  #${GREEN}%&&&&%%%%%%%%%%%%${NORMAL}
      ${BLUE}#######((((###%%%%%${NORMAL}#############${GREEN}&%%%%%%%${NORMAL}
      ${BLUE}######/     ########%%%%&&&&&&&%%${NORMAL}
       ${BLUE}((((((((((((######%%%%%%%${NORMAL}
@@ -33,26 +33,75 @@ while [ -z "$VERSION" ]; do
     read -r VERSION
 done
 until [ "$ARCH" = 'amd64' ] || [ "$ARCH" = 'i686' ] || [ "$ARCH" = 'aarch64' ] || [ "$ARCH" = 'armv7' ]; do
-    echo '1 x86_64'
-    echo '2 x86'
+    echo '1 amd64'
+    echo '2 i686'
     echo '3 aarch64'
     echo '4 armv7'
     printf 'Which architecture? amd64 (default), i686, or aarch64 or armv7'
     read -r input_arch
-    [ "$input_arch" = 1 ] && ARCH='x86_64'
-    [ "$input_arch" = 2 ] && ARCH='x86'
+    [ "$input_arch" = 1 ] && ARCH='amd64'
+    [ "$input_arch" = 2 ] && ARCH='i686'
     [ "$input_arch" = 3 ] && ARCH='aarch64'
-    [ "$input_arch" = 4 ] && ARCH='x86'
-    [ -z "$input_arch" ] && ARCH='x86_64'
+    [ "$input_arch" = 4 ] && ARCH='armv7'
+    [ -z "$input_arch" ] && ARCH='amd64'
 done
 
 # Install dependencies to build palen1x
 apt-get update
 apt-get install -y --no-install-recommends git wget gawk debootstrap mtools xorriso ca-certificates curl libusb-1.0-0-dev gcc make gzip xz-utils unzip libc6-dev coreutils exfat-utils
 
-# RootFS and Downra1n
-ROOTFS="https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/$ARCH/alpine-minirootfs-3.17.3-$ARCH.tar.gz"
-DOWNRA1N='https://github.com/edwin170/downr1n.git'
+# Get proper files
+if [ "$1" = "RELEASE" ]; then
+    case "$ARCH" in
+        'amd64')
+            ROOTFS='https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/x86_64/alpine-minirootfs-3.17.3-x86_64.tar.gz'
+            PALERA1N="https://github.com/palera1n/palera1n/releases/download/v2.0.0-beta.7/palera1n-linux-x86_64"
+            ;;
+        'i686')
+            ROOTFS='https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/x86/alpine-minirootfs-3.17.3-x86.tar.gz'
+            PALERA1N="https://github.com/palera1n/palera1n/releases/download/v2.0.0-beta.7/palera1n-linux-x86"
+            ;;
+        'aarch64')
+            ROOTFS='https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/aarch64/alpine-minirootfs-3.17.3-aarch64.tar.gz'
+            PALERA1N="https://github.com/palera1n/palera1n/releases/download/v2.0.0-beta.7/palera1n-linux-arm64"
+            ;;
+        'armv7')
+            ROOTFS='https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/armv7/alpine-minirootfs-3.17.3-armv7.tar.gz'
+            PALERA1N="https://github.com/palera1n/palera1n/releases/download/v2.0.0-beta.7/palera1n-linux-armel"
+            ;;
+    esac
+    echo "INFO: RELEASE CHOSEN"
+elif [ "$1" = "NIGHTLY" ]; then
+
+
+    url="https://cdn.nickchan.lol/palera1n/artifacts/c-rewrite/main/"
+    latest_build=0
+    html=$(curl -s "$url")
+    latest_build=$(echo "$html" | awk -F'href="' '{print $2}' | awk -F'/' 'NF>1{print $1}' | sort -nr | head -1)
+
+     case "$ARCH" in
+        'amd64')
+            ROOTFS='https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/x86_64/alpine-minirootfs-3.17.3-x86_64.tar.gz'
+            PALERA1N="https://cdn.nickchan.lol/palera1n/artifacts/c-rewrite/main/$latest_build/palera1n-linux-x86_64"
+            ;;
+        'i686')
+            ROOTFS='https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/x86/alpine-minirootfs-3.17.3-x86.tar.gz'
+            PALERA1N="https://cdn.nickchan.lol/palera1n/artifacts/c-rewrite/main/$latest_build/palera1n-linux-x86"
+            ;;
+        'aarch64')
+            ROOTFS='https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/aarch64/alpine-minirootfs-3.17.3-aarch64.tar.gz'
+            PALERA1N="https://cdn.nickchan.lol/palera1n/artifacts/c-rewrite/main/$latest_build/palera1n-linux-arm64"
+            ;;
+        'armv7')
+            ROOTFS='https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/armv7/alpine-minirootfs-3.17.3-armv7.tar.gz'
+            PALERA1N="https://cdn.nickchan.lol/palera1n/artifacts/c-rewrite/main/$latest_build/palera1n-linux-armel"
+            ;;
+    esac
+    echo "INFO: NIGHTLY CHOSEN"
+elif [[ -z "$BUILD_TYPE" ]]; then
+    echo "ERROR: NO BUILD TYPE CHOSEN"
+    exit 1
+fi
 
 # Clean up previous attempts
 umount -v work/rootfs/{dev,sys,proc} >/dev/null 2>&1
@@ -87,7 +136,7 @@ rc-update add udev-settle
 !
 
 # kernel modules
-cat << ! > rootfs/etc/mkinitfs/features.d/down1x.modules
+cat << ! > rootfs/etc/mkinitfs/features.d/palen1x.modules
 kernel/drivers/usb/host
 kernel/drivers/hid/usbhid
 kernel/drivers/hid/hid-generic.ko
@@ -96,16 +145,16 @@ kernel/drivers/hid/hid-apple.ko
 kernel/net/ipv4
 !
 chroot rootfs /usr/bin/env PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin \
-	/sbin/mkinitfs -F "down1x" -k -t /tmp -q $(ls rootfs/lib/modules)
+	/sbin/mkinitfs -F "palen1x" -k -t /tmp -q $(ls rootfs/lib/modules)
 rm -rfv rootfs/lib/modules
 mv -v rootfs/tmp/lib/modules rootfs/lib
 find rootfs/lib/modules/* -type f -name "*.ko" -exec strip -v --strip-unneeded {} \; -exec xz --x86 -v9eT0 \;
 depmod -b rootfs $(ls rootfs/lib/modules)
 
 # Echo TUI configurations
-echo 'down1x' > rootfs/etc/hostname
+echo 'palen1x' > rootfs/etc/hostname
 echo "PATH=$PATH:$HOME/.local/bin" > rootfs/root/.bashrc # d
-echo "export DOWN1X_VERSION='$VERSION'" > rootfs/root/.bashrc
+echo "export PALEN1X_VERSION='$VERSION'" > rootfs/root/.bashrc
 echo '/usr/bin/palen1x_menu' >> rootfs/root/.bashrc
 echo "Rootless" > rootfs/usr/bin/.jbtype
 echo "" > rootfs/usr/bin/.args
@@ -128,7 +177,7 @@ ln -sv ../../etc/terminfo rootfs/usr/share/terminfo # fix ncurses
 cp -av rootfs/boot/vmlinuz-lts iso/boot/vmlinuz
 cat << ! > iso/boot/grub/grub.cfg
 insmod all_video
-echo 'down1x $VERSION'
+echo 'palen1x $VERSION'
 linux /boot/vmlinuz quiet loglevel=3
 initrd /boot/initramfs.xz
 boot
